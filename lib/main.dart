1a2e9684a4fe6db04b83bc29dcd3a15b48e2ee00
Osama Bin Hashim <zeeshan2k01@gmail.com>
2024-01-20 18:03:24 +0900

    title set

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return Text("This is a test", textDirection: TextDirection.ltr);
-    return const MaterialApp(home: CurrencyConverterMaterial());
+    return const MaterialApp(
+        title: "USD to TK converter", home: CurrencyConverterMaterial());
   }
 }
